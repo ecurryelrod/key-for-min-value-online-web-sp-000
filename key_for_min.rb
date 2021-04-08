24559e579e3a35795be619866d50f4216a16d978
Erika Curry-Elrod <ecurryelrod@gmail.com>
2021-04-08 17:30:57 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   min_value = 0
   min_name = nil
   name_hash.each do |name, value|
-    if value <= min_value
+    if min_value == 0 || value < min_value
       min_value = value 
       min_name = name
     end
